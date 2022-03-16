f567dd8cb5b5b40a12bbb8fc9e38b8e9b5ac9e80
unknown <jenn@example.com>
2022-03-16 14:56:07 -0600

    fixed bugs

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello and welcome to the Jungle Journey! You have been invited by a friend in a nearby village to join a celebration. You will venture down the path and make choices along the way!");
+
+
+
+
+
+
+
+
+
+
+
